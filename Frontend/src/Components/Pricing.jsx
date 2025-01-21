857366114e2e6a498d996d0011260d6f28ea84c6
Sapana Dashoni <learnersapana100@gmail.com>
2025-01-21 15:58:25 +0530

    changes in pricing

--- Frontend/src/Components/Pricing.jsx
+++ Frontend/src/Components/Pricing.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { Check } from 'lucide-react'; // Replace 'your-library' with the correct import
-import { Link } from 'react-router-dom';
+import { Check } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Pricing = () => {
   const pricing = [
     {
       imgUrl: "/pricing.jpg",
-      title: "QUARTERLY PLAN",
+      title: "QUARTERLY",
       price: 18000,
-      length: 3
+      length: 3,
     },
     {
       imgUrl: "/pricing.jpg",
-      title: "HALF YEARLY PLAN",
+      title: "HEAL_YEARLY",
       price: 34000,
-      length: 6
+      length: 6,
     },
     {
       imgUrl: "/pricing.jpg",
-      title: "YEARLY PLAN",
+      title: "YEARLY",
       price: 67000,
-      length: 12
-    }
+      length: 12,
+    },
   ];
-
   return (
     <section className="pricing">
       <h1>ELITE EDGE FITNESS PLANS</h1>
@@ -38,16 +37,15 @@ const Pricing = () => {
                 <h3>Rs {element.price}</h3>
                 <p>For {element.length} Months</p>
               </div>
-
               <div className="description">
                 <p>
                   <Check /> Equipment
                 </p>
                 <p>
-                  <Check /> All day free Training
+                  <Check /> All Day Fre Training
                 </p>
                 <p>
-                  <Check /> Free restroom
+                  <Check /> Free Restroom
                 </p>
                 <p>
                   <Check /> 24/7 Skilled Support
